845f8fc053f4ca660e9e1cf445ecc3c1e85fe765
Marjan Khamesian <marjan.khamesian@kemi.uu.se>
2021-01-07 15:36:04 +0100

    initializing

---
+++ ab_test.py
@@ -0,0 +1,14 @@
+# This program provides a walkthrough of the A/B testing course's final
+# project, which is provided by Udacity in collaboration with Google.
+
+#======================================================================
+# Author: Marjan Khamesian
+# Date: January 2021
+#======================================================================
+
+import math as mt
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
+
+
