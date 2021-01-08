a3b193d463de1acab1e222cf2f3508dab24c634d
Your Name <khamesian.marjan@gmail.com>
2021-01-08 10:26:47 +0100

    metrics

--- ab_test.py
+++ ab_test.py
@@ -1,5 +1,5 @@
 # This program provides a walkthrough of the A/B testing course's final
-# project, which is provided by Udacity in collaboration with Google.
+# project provided by Udacity in collaboration with Google.
 
 #======================================================================
 # Author: Marjan Khamesian
@@ -11,16 +11,17 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm
 
-# baseline data: placing - the estimators gievn by Udacity - into a dictionary
+# === Baseline data
+# placing - the estimators gievn by Udacity - into a dictionary
 baseline = {"Cookies":40000,"Clicks":3200,"Enrollments":660,"CTP":0.08,"GConversion":0.20625,
            "Retention":0.53,"NConversion":0.109313}
 
 
 # === Estimating Analytically 
-# getting the p and n needed for Gross Conversion (GC), Retention(R) & Net Conversion (NC)
-# and computing the Stansard Deviation(sd) rounded to 4 decimal digits.
+# getting the p (probability) and n (sample size) needed for different metrics.
+# computing the Stansard Deviation (sd).
 
-# Gross Conversion 
+# Gross Conversion (GC)
 GC={}
 GC["d_min"]=0.01
 GC["p"]=baseline["GConversion"]
@@ -28,7 +29,7 @@ GC["n"]=baseline["Clicks"]
 GC["sd"]=round(mt.sqrt((GC["p"]*(1-GC["p"]))/GC["n"]),4)
 GC["sd"]
 
-# Retention
+# Retention (R)
 R={}
 R["d_min"]=0.01
 R["p"]=baseline["Retention"]
@@ -36,7 +37,7 @@ R["n"]=baseline["Enrollments"]
 R["sd"]=round(mt.sqrt((R["p"]*(1-R["p"]))/R["n"]),4)
 R["sd"]
 
-# Net Conversion
+# Net Conversion (NC)
 NC={}
 NC["d_min"]=0.0075
 NC["p"]=baseline["NConversion"]
